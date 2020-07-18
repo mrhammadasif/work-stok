c1124eff283bb2744e5dfd5e7abb6ced05192ee9
Hammad Asif <mrhammadasif@gmail.com>
2020-07-18 12:58:48 +0500

    finalized app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <p class="text-center">{{key}} - {{rows}} - {{cols}}</p>
+    <div class="p-4 g-4 d-flex align-items-center">
+      <label class="p-0 m-0" for="">Rows</label>
+      <input v-model.number="rows" :min="2" :max="20" type="range" class="form-control ml-2 mr-4 m-0">
+      <label class="p-0 m-0" for="">Columns</label>
+      <input v-model.number="cols" min="2" step="1" max="20" type="range" class="form-control ml-2 m-0">
+      <button class="btn btn-primary ml-4" @click="regenKey">Regenerate!</button>
+    </div>
+    <table class="table-bordered" :key="key">
+      <tr v-for="row in rows" :key="key + row">
+        <cell v-for="col in cols" :value="key" :key="key + row + col" />
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import 'bootstrap/dist/css/bootstrap.css'
+import Cell from './components/Cell.vue'
+const chance = require('chance')()
 export default {
   name: 'App',
+  data () {
+    return {
+      key: '1231232c',
+      rows: 4,
+      cols: 4
+    }
+  },
+  methods: {
+    regenKey () {
+      this.key = chance.guid()
+    }
+  },
   components: {
-    HelloWorld
+    Cell
   }
 }
 </script>
---
+++ src/components/Cell.vue
@@ -0,0 +1,22 @@
+<template>
+  <td class="me">{{something.toUpperCase()}}</td>
+</template>
+<script>
+const chance = require('chance')()
+export default {
+  props: ['value'],
+  computed: {
+    something () {
+      return chance.letter()
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.me {
+  width: 40px;
+  height: 40px;
+  font-weight: bold;
+  font-size: 20px;
+}
+</style>
